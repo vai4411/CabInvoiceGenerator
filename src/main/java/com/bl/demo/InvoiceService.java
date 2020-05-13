c7411e2ddc57dec9f004291ca678dd24a53d2f23
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-05-13 22:59:17 +0530

    ADDED: Given Cab Agency Now Support Two Categories

--- src/main/java/com/bl/demo/InvoiceService.java
+++ src/main/java/com/bl/demo/InvoiceService.java
@@ -1,41 +1,51 @@
 package com.bl.demo;
 
 public class InvoiceService {
-    private static final double COST_PER_KILOMETER = 10;
-    private static final int COST_PER_MINUTE = 1;
-    private static final int MINIMUM_COST = 5;
+    private double costPerKiloMeter;
+    private int costPerMinute;
+    private int minimumCost;
     private RideRepository rideRepository;
 
     public InvoiceService() {
         this.rideRepository = new RideRepository();
     }
 
-    public double calculateFare(double distance, int time) {
-        double fare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
-        return Math.max(fare,MINIMUM_COST);
+    public double calculateFare(double distance, int time, String category) {
+        if (category == "Normal") {
+            costPerKiloMeter = 10.0;
+            costPerMinute = 1;
+            minimumCost = 5;
+        }
+        else {
+            costPerKiloMeter = 15.0;
+            costPerMinute = 2;
+            minimumCost = 20;
+        }
+        double fare = distance * costPerKiloMeter + time * costPerMinute;
+        return Math.max(fare, minimumCost);
     }
 
-    public double giveFare(Ride[] rides) {
+    public double giveFare(Ride[] rides, String category) {
         double totalFare = 0;
         for (Ride ride:rides) {
-            totalFare += this.calculateFare(ride.distance,ride.time);
+            totalFare += this.calculateFare(ride.distance,ride.time,category);
         }
         return totalFare;
     }
 
-    public InvoiceSummary calculateFare(Ride[] rides) {
-        return new InvoiceSummary(rides.length,this.giveFare(rides));
+    public InvoiceSummary calculateFare(Ride[] rides,String category) {
+        return new InvoiceSummary(rides.length,this.giveFare(rides,category));
     }
 
-    public double calculateFares(Ride[] rides) {
-        return this.giveFare(rides);
+    public double calculateFares(Ride[] rides,String category) {
+        return this.giveFare(rides,category);
     }
 
     public void addRides(String userId, Ride[] rides) {
         rideRepository.addRides(userId,rides);
     }
 
-    public InvoiceSummary getInvoiceSummary(String userId) {
-        return this.calculateFare(rideRepository.getRides(userId));
+    public InvoiceSummary getInvoiceSummary(String userId,String category) {
+        return this.calculateFare(rideRepository.getRides(userId),category);
     }
 }
--- src/test/java/com/bl/demo/InvoiceServiceTest.java
+++ src/test/java/com/bl/demo/InvoiceServiceTest.java
@@ -16,7 +16,7 @@ public class InvoiceServiceTest {
     public void givenDistanceAndTime_ShouldReturnFare() {
        double distance = 2.0;
        int time = 5;
-       double fare = invoiceService.calculateFare(distance,time);
+       double fare = invoiceService.calculateFare(distance,time,"Normal");
        Assert.assertEquals(25,fare,0.0);
    }
 
@@ -24,7 +24,7 @@ public class InvoiceServiceTest {
    public void givenMinimumDistanceAndTime_ShouldReturnFare() {
       double distance = 0.1;
       int time = 1;
-      double fare = invoiceService.calculateFare(distance,time);
+      double fare = invoiceService.calculateFare(distance,time,"Normal");
       Assert.assertEquals(5,fare,0.0);
    }
 
@@ -33,7 +33,7 @@ public class InvoiceServiceTest {
       Ride[] rides = { new Ride(2.0, 5),
                        new Ride(0.1,1)
                      };
-      double fare = invoiceService.calculateFares(rides);
+      double fare = invoiceService.calculateFares(rides,"Normal");
       Assert.assertEquals(30,fare,0.0);
    }
 
@@ -42,7 +42,7 @@ public class InvoiceServiceTest {
       Ride[] rides = { new Ride(2.0, 5),
               new Ride(0.1,1)
       };
-      InvoiceSummary summary = invoiceService.calculateFare(rides);
+      InvoiceSummary summary = invoiceService.calculateFare(rides,"Normal");
       InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
       Assert.assertEquals(expectedInvoiceSummary,summary);
    }
@@ -54,8 +54,56 @@ public class InvoiceServiceTest {
               new Ride(0.1,1)
       };
       invoiceService.addRides(userId,rides);
-      InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
+      InvoiceSummary summary = invoiceService.getInvoiceSummary(userId,"Normal");
       InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
       Assert.assertEquals(expectedInvoiceSummary,summary);
    }
+
+   @Test
+   public void givenDistanceAndTime_ForPremiumRide_ShouldReturnFare() {
+      double distance = 2.0;
+      int time = 5;
+      double fare = invoiceService.calculateFare(distance,time,"Premium");
+      Assert.assertEquals(40,fare,0.0);
+   }
+
+   @Test
+   public void givenMinimumDistanceAndTime_ForPremiumRide_ShouldReturnFare() {
+      double distance = 0.1;
+      int time = 1;
+      double fare = invoiceService.calculateFare(distance,time,"Premium");
+      Assert.assertEquals(20,fare,0.0);
+   }
+
+   @Test
+   public void givenMultipleRides_ForPremiumRide_ShouldReturnTotalFare() {
+      Ride[] rides = { new Ride(2.0, 5),
+              new Ride(0.1,1)
+      };
+      double fare = invoiceService.calculateFares(rides,"Premium");
+      Assert.assertEquals(60,fare,0.0);
+   }
+
+   @Test
+   public void givenMultipleRides_ForPremiumRide_ShouldReturnInvoiceSummary() {
+      Ride[] rides = { new Ride(2.0, 5),
+              new Ride(0.1,1)
+      };
+      InvoiceSummary summary = invoiceService.calculateFare(rides,"Premium");
+      InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 60.0);
+      Assert.assertEquals(expectedInvoiceSummary,summary);
+   }
+
+   @Test
+   public void givenUserIdAndRides_ForPremiumRide_ShouldReturnInvoiceSummary() {
+      String userId = "a@b.com";
+      Ride[] rides = { new Ride(2.0, 5),
+              new Ride(0.1,1)
+      };
+      invoiceService.addRides(userId,rides);
+      InvoiceSummary summary = invoiceService.getInvoiceSummary(userId,"Premium");
+      InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 60.0);
+      Assert.assertEquals(expectedInvoiceSummary,summary);
+   }
+
 }
