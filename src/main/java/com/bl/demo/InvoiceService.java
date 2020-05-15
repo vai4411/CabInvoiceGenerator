60df879641c11f29c967489fc2174f629e93c8a1
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-05-15 13:40:36 +0530

    REFACTORED: Change String Parameter To Boolean For Category Check

--- src/main/java/com/bl/demo/InvoiceService.java
+++ src/main/java/com/bl/demo/InvoiceService.java
@@ -10,42 +10,42 @@ public class InvoiceService {
         this.rideRepository = new RideRepository();
     }
 
-    public double calculateFare(double distance, int time, String category) {
-        if (category == "Normal") {
-            costPerKiloMeter = 10.0;
-            costPerMinute = 1;
-            minimumCost = 5;
-        }
-        else {
+    public double calculateFare(double distance, int time, boolean isPremium) {
+        if (isPremium) {
             costPerKiloMeter = 15.0;
             costPerMinute = 2;
             minimumCost = 20;
+        }
+        else {
+            costPerKiloMeter = 10.0;
+            costPerMinute = 1;
+            minimumCost = 5;
         }
         double fare = distance * costPerKiloMeter + time * costPerMinute;
         return Math.max(fare, minimumCost);
     }
 
-    public double giveFare(Ride[] rides, String category) {
+    public double giveFare(Ride[] rides, boolean isPremium) {
         double totalFare = 0;
         for (Ride ride:rides) {
-            totalFare += this.calculateFare(ride.distance,ride.time,category);
+            totalFare += this.calculateFare(ride.distance,ride.time,isPremium);
         }
         return totalFare;
     }
 
-    public InvoiceSummary calculateFare(Ride[] rides,String category) {
-        return new InvoiceSummary(rides.length,this.giveFare(rides,category));
+    public InvoiceSummary calculateFare(Ride[] rides,boolean isPremium) {
+        return new InvoiceSummary(rides.length,this.giveFare(rides,isPremium));
     }
 
-    public double calculateFares(Ride[] rides,String category) {
-        return this.giveFare(rides,category);
+    public double calculateFares(Ride[] rides,boolean isPremium) {
+        return this.giveFare(rides,isPremium);
     }
 
     public void addRides(String userId, Ride[] rides) {
         rideRepository.addRides(userId,rides);
     }
 
-    public InvoiceSummary getInvoiceSummary(String userId,String category) {
-        return this.calculateFare(rideRepository.getRides(userId),category);
+    public InvoiceSummary getInvoiceSummary(String userId,boolean isPremium) {
+        return this.calculateFare(rideRepository.getRides(userId),isPremium);
     }
 }
--- src/test/java/com/bl/demo/InvoiceServiceTest.java
+++ src/test/java/com/bl/demo/InvoiceServiceTest.java
@@ -13,97 +13,96 @@ public class InvoiceServiceTest {
    }
 
    @Test
-    public void givenDistanceAndTime_ShouldReturnFare() {
+    public void givenDistanceAndTime_WhenNormalRide_ThenShouldReturnFare() {
        double distance = 2.0;
        int time = 5;
-       double fare = invoiceService.calculateFare(distance,time,"Normal");
+       double fare = invoiceService.calculateFare(distance,time,false);
        Assert.assertEquals(25,fare,0.0);
    }
 
    @Test
-   public void givenMinimumDistanceAndTime_ShouldReturnFare() {
+   public void givenMinimumDistanceAndTime_WhenNormalRide_ThenShouldReturnFare() {
       double distance = 0.1;
       int time = 1;
-      double fare = invoiceService.calculateFare(distance,time,"Normal");
+      double fare = invoiceService.calculateFare(distance,time,false);
       Assert.assertEquals(5,fare,0.0);
    }
 
    @Test
-   public void givenMultipleRides_ShouldReturnTotalFare() {
+   public void givenMultipleRides_WhenNormalRide_ThenShouldReturnTotalFare() {
       Ride[] rides = { new Ride(2.0, 5),
                        new Ride(0.1,1)
                      };
-      double fare = invoiceService.calculateFares(rides,"Normal");
+      double fare = invoiceService.calculateFares(rides,false);
       Assert.assertEquals(30,fare,0.0);
    }
 
    @Test
-   public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+   public void givenMultipleRides_WhenNormalRide_ThenShouldReturnInvoiceSummary() {
       Ride[] rides = { new Ride(2.0, 5),
               new Ride(0.1,1)
       };
-      InvoiceSummary summary = invoiceService.calculateFare(rides,"Normal");
+      InvoiceSummary summary = invoiceService.calculateFare(rides,false);
       InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
       Assert.assertEquals(expectedInvoiceSummary,summary);
    }
 
    @Test
-   public void givenUserIdAndRides_ShouldReturnInvoiceSummary() {
+   public void givenUserIdAndRides_WhenNormalRide_ThenShouldReturnInvoiceSummary() {
       String userId = "a@b.com";
       Ride[] rides = { new Ride(2.0, 5),
               new Ride(0.1,1)
       };
       invoiceService.addRides(userId,rides);
-      InvoiceSummary summary = invoiceService.getInvoiceSummary(userId,"Normal");
+      InvoiceSummary summary = invoiceService.getInvoiceSummary(userId,false);
       InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
       Assert.assertEquals(expectedInvoiceSummary,summary);
    }
 
    @Test
-   public void givenDistanceAndTime_ForPremiumRide_ShouldReturnFare() {
+   public void givenDistanceAndTime_WhenForPremiumRide_ThenShouldReturnFare() {
       double distance = 2.0;
       int time = 5;
-      double fare = invoiceService.calculateFare(distance,time,"Premium");
+      double fare = invoiceService.calculateFare(distance,time,true);
       Assert.assertEquals(40,fare,0.0);
    }
 
    @Test
-   public void givenMinimumDistanceAndTime_ForPremiumRide_ShouldReturnFare() {
+   public void givenMinimumDistanceAndTime_WhenForPremiumRide_ThenShouldReturnFare() {
       double distance = 0.1;
       int time = 1;
-      double fare = invoiceService.calculateFare(distance,time,"Premium");
+      double fare = invoiceService.calculateFare(distance,time,true);
       Assert.assertEquals(20,fare,0.0);
    }
 
    @Test
-   public void givenMultipleRides_ForPremiumRide_ShouldReturnTotalFare() {
+   public void givenMultipleRides_WhenForPremiumRide_ThenShouldReturnTotalFare() {
       Ride[] rides = { new Ride(2.0, 5),
               new Ride(0.1,1)
       };
-      double fare = invoiceService.calculateFares(rides,"Premium");
+      double fare = invoiceService.calculateFares(rides,true);
       Assert.assertEquals(60,fare,0.0);
    }
 
    @Test
-   public void givenMultipleRides_ForPremiumRide_ShouldReturnInvoiceSummary() {
+   public void givenMultipleRides_WhenForPremiumRide_ThenShouldReturnInvoiceSummary() {
       Ride[] rides = { new Ride(2.0, 5),
               new Ride(0.1,1)
       };
-      InvoiceSummary summary = invoiceService.calculateFare(rides,"Premium");
+      InvoiceSummary summary = invoiceService.calculateFare(rides,true);
       InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 60.0);
       Assert.assertEquals(expectedInvoiceSummary,summary);
    }
 
    @Test
-   public void givenUserIdAndRides_ForPremiumRide_ShouldReturnInvoiceSummary() {
+   public void givenUserIdAndRides_WhenForPremiumRide_ThenShouldReturnInvoiceSummary() {
       String userId = "a@b.com";
       Ride[] rides = { new Ride(2.0, 5),
               new Ride(0.1,1)
       };
       invoiceService.addRides(userId,rides);
-      InvoiceSummary summary = invoiceService.getInvoiceSummary(userId,"Premium");
+      InvoiceSummary summary = invoiceService.getInvoiceSummary(userId,true);
       InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 60.0);
       Assert.assertEquals(expectedInvoiceSummary,summary);
    }
-
 }
